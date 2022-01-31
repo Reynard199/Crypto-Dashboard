e6dcbcc1411c36ada1d32865e92267680340057b
Reynard199 <64665998+Reynard199@users.noreply.github.com>
2022-01-31 16:33:40 +0200

    Add files via upload

--- CS_CryptoDashBoard.py
+++ CS_CryptoDashBoard.py
@@ -69,8 +69,11 @@ def get_data(symbol, start_date, end_date) :
         df = pd.DataFrame(columns = ['Date', 'Close', 'Open', 'High', 'Low', 'Adj Close', 'Volume'])
     
     df['Date'] = df.index
+    
     #df['Date'] = pd.to_datetime(df['Date'])
     
+    df['Year'] = df.Date.dt.year
+    df['Month'] = df.Date.dt.month
     df['Returns'] = (df['Close'] / df["Close"].iloc[0] - 1) * 100
 
     df = df.set_index(pd.DatetimeIndex(df['Date'].values))
@@ -94,27 +97,42 @@ fig = go.Figure(
         decreasing_line_color = 'red'
         )])
 
+st.markdown("***")
+
 st.header(crypto_name + ' Data')
 st.dataframe(df.sort_values(by = 'Date', ascending=False).drop(columns = 'Date'))
+
+st.markdown("***")
 
 st.header(crypto_name + ' Closing Price Statistics')
 st.dataframe(df.groupby(df.Date.dt.year).Close.describe().transpose(), width = 1200)
 
+st.markdown("***")
+
 st.header(crypto_name + ' Returns Statistics')
 st.dataframe(df.groupby(df.Date.dt.year).Returns.describe().transpose(), width = 2000)
+
+st.markdown("***")
 
 st.header(crypto_name + ' Close Price')
 st.line_chart(df['Close'])
 
+st.markdown("***")
+
 st.header(crypto_name + ' Volume')
 st.bar_chart(df['Volume'])
 
-st.header(crypto_name + ' Candle Stick')
-st.plotly_chart(fig, use_container_width=True)
+st.markdown("***")
 
+st.header(crypto_name + ' Candle Stick')
+candle_stick = st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("***")
 st.header(crypto_name + ' : Moving Averages Trading Strategies')
-# st.line_chart(trading_df['Adj Close'])
-# st.pyplot(moving_averages_2(moving_averages))
+st.markdown("A SMA-EWM Trading Strategy has simple underlying principles. When the EWM (Exponential Weighted Moving Average) crosses the SMA (Simple Moving Average), " + str(crypto_name) + " is bought at the crossover price, indicated in green. The opposite action is taken if the EWM crosses the SMA, indicated in red. The number of periods that the weighted averages are calculated over is controlled by the 'Moving Averages Time Period' slider, in this case being " + str(moving_averages) + " days.")
+st.markdown("    ")
+st.markdown("General Note - This trading strategy is rarely effective (Thanks Weak Efficient Market Hypothesis), but it was certainly interesting to code")
+st.markdown("***")
 
 def trading(moving_averages) : 
     trading_df = pd.DataFrame()
@@ -141,19 +159,23 @@ def trading(moving_averages) :
     fig.add_trace(go.Scatter(x = df['Date'], y = df['Close'], mode = 'lines', line=dict(color='royalblue', width=2), name = 'Closing Price'))
     fig.add_trace(go.Scatter(x = df['Date'], y = trading_df['Buy'], mode = 'markers', name = 'Buy', marker=dict(color='green', size =7)))
     fig.add_trace(go.Scatter(x = df['Date'], y = trading_df['Sell'], mode = 'markers', name = 'Sell', marker=dict(color='red', size =7)))
-    fig.update_layout(#autosize = True,
-            # width = 1200, height = 600,
-            title = ("Moving Simple and Exponential Trading Strategy Applied Over " + str(moving_averages) + " Days == $" + str(profit) + " Return between the dates " + str(start) + ' and ' + str(end)),
+    fig.update_layout(autosize = False,
+            width = 1200, height = 600,
+            title_x = 0.5,
+            title = ("Moving Simple and Exponential Trading Strategy Applied Over " + str(moving_averages) + " Days = $" + str(profit) +" Return <br> [between the dates " + str(start) + ' and ' + str(end) + "]"),
             xaxis_title = ("Date Range between " + str(start) + ' and ' + str(end)),
             yaxis_title = "Price in USD",
             legend_title = "Legend",
             title_font=dict(
                 family = "Bradley Hand",
-                size = 17),
+                size = 20),
             font = dict(family = "Bradley Hand",
-                    size = 16)
+                    size = 16),
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)'
             )
     st.plotly_chart(fig)
+    st.markdown("***")
     
     return trading_df, profit
 
