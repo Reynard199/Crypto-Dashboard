6f4a28fce738e96dbd3273a1d6688fc713789d02
Reynard199 <64665998+Reynard199@users.noreply.github.com>
2022-02-21 17:32:57 +0200

    Update CS_CryptoDashBoard.py

--- CS_CryptoDashBoard.py
+++ CS_CryptoDashBoard.py
@@ -43,8 +43,8 @@ def get_input():
     crypto_symbol = st.sidebar.selectbox('Crypto Coin', options = ['BTC-USD', 'DOGE-USD', 'ETH-USD'])
     with st.sidebar.expander('Note about Date Selection', expanded = False) :
         st.write('Cryptos have data for weekends and public holidays (New Years Day etc), while stocks do not. Please select a week day to receive the greatest comparison functionality. Thanks!')
-    start_date = st.sidebar.date_input("Start Date", value = datetime.date(2021,1,1), max_value = (datetime.date.today() - datetime.timedelta(days = 1)))
-    end_date = st.sidebar.date_input("End Date", value = datetime.date.today(), max_value = datetime.date.today())
+    start_date = st.sidebar.date_input("Start Date", value = datetime.date(2021,1,1), max_value = (datetime.date.today() - datetime.timedelta(days = 1)), min_value = datetime.date(2015,1,1))
+    end_date = st.sidebar.date_input("End Date", value = datetime.date.today(), max_value = datetime.date.today(), min_value = start_date + datetime.timedelta(days = 1))
     selected_stock = st.sidebar.text_input('Select a Ticker as per the Yahoo Finance Ticker Format (ABG.JO is ABSA)', 'ABG.JO').upper()
     ticker_list = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'][1:-1]
     ticker_list = ticker_list.append(pd.Series(['^GSPC', 'ETH-USD', 'DOGE-USD', 'BTC-USD', '^J203.JO', selected_stock]))
