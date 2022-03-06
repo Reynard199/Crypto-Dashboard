8d284f7da5bcb0d652c1e779860ebb5541822d8c
Reynard199 <64665998+Reynard199@users.noreply.github.com>
2022-03-06 17:29:54 +0200

    Update CS_CryptoDashBoard.py

--- CS_CryptoDashBoard.py
+++ CS_CryptoDashBoard.py
@@ -47,7 +47,7 @@ def get_input():
     crypto_symbol = st.sidebar.selectbox('Crypto Coin or Commodity', options = ['BTC-USD', 'DOGE-USD', 'ETH-USD', 'GC=F', 'CL=F'])
     with st.sidebar.expander('Note about Date Selection', expanded = False) :
         st.write('Some of the tickers have different dates when they where created, so please use an ending value later than 1 Jan 2018. Otherwise it breaks the dash. Thanks!')
-    start_date = st.sidebar.date_input("Start Date", value = datetime.date(2021,1,1), max_value = (datetime.date.today() - datetime.timedelta(days = 1)), min_value = datetime.date(2015,1,1))
+    start_date = st.sidebar.date_input("Start Date", value = datetime.date(2021,1,1), max_value = (datetime.date.today() - datetime.timedelta(days = 30)), min_value = datetime.date(2015,1,1))
     end_date = st.sidebar.date_input("End Date", value = datetime.date.today(), max_value = datetime.date.today(), min_value = start_date + datetime.timedelta(days = 30))
     selected_stock = st.sidebar.text_input('Select a Ticker as per the Yahoo Finance Ticker Format (ABG.JO is ABSA)', 'ABG.JO').upper()
     ticker_list = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'][1:-1]
